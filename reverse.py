e1eeb64a652d9946c1dece9dca8e18946fc2b983
Aahaan01 <160381765+Aahaan01@users.noreply.github.com>
2024-10-15 19:08:18 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,5 @@
+name=input("Enter your name ")
+name2=""
+for i in name:
+    name2=i+name2
+print(name2, "is your name in reverse!")
---
+++ sum.py
@@ -0,0 +1,6 @@
+n=int(input("Please enter a number "))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+    
+print("sum is", sum)
